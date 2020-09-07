0c55a745eb8a76fc5e37f03c07d1e916bc6ef27e
Silvija Prozinger <37271365+SilvijaProzinger@users.noreply.github.com>
2020-09-07 12:20:26 +0200

    fixed code indentation a bit

--- js/app.js
+++ js/app.js
@@ -1,16 +1,14 @@
-/*
- * Create a list that holds all of your cards
- */
+// Create a list that holds all cards
 const cardList = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt", "fa fa-cube", "fa fa-cube", "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle", "fa fa-bomb", "fa fa-bomb"];
 
-//Create deck that holds all the cards
+// Create deck that holds all the cards
 const deck = document.querySelector(".deck");
 
-//Create an empty array for opened cards
+// Create an empty array for opened cards
 let openedCards = [];
 let matchedCards = [];
 
-//Move counter variables
+// Move counter variables
 let moveCounter = document.querySelector(".moves");
 let moves = 0;
 
@@ -18,192 +16,187 @@ let moves = 0;
 const stars = document.querySelector(".stars");
 stars.innerHTML = '<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>';
 
-//Timer variables
+// Timer variables
 let timer = document.querySelector(".timer")
 let second = 0;
 let startTime;
 
-//Modal variables
+// Modal variables
 let modal = document.getElementById("modal");
 let replay = document.querySelector("#replay");
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+ var currentIndex = array.length, temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+ while (currentIndex !== 0) {
+  randomIndex = Math.floor(Math.random() * currentIndex);
+  currentIndex -= 1;
+  temporaryValue = array[currentIndex];
+  array[currentIndex] = array[randomIndex];
+  array[randomIndex] = temporaryValue;
+ }
 
-    return array;
+ return array;
 }
 
-//Create cards and add them to the deck
+// Create cards and add them to the deck
 function start(){
-	for(let i = 0; i < cardList.length; i++) {
-        let card = document.createElement("li");
-        card.classList.add("card");
-        card.innerHTML = `<i class="${cardList[i]}"></i>`;
-        deck.appendChild(card);
+ for(let i = 0; i < cardList.length; i++) {
+  let card = document.createElement("li");
+  card.classList.add("card");
+  card.innerHTML = `<i class="${cardList[i]}"></i>`;
+  deck.appendChild(card);
 
-        //Invoke clickOnCards function
-        clickOnCards(card);
-    }
-		// Enable the timer to reset to 0 when the game is restarted
-		resetTimer(startTime);
-		second = 0;
-		timer.textContent =`${second}`;
-		timerStart();
+  // Invoke clickOnCards function
+  clickOnCards(card);
+ }
 
+ // Enable the timer to reset to 0 when the game is restarted
+ resetTimer(startTime);
+ second = 0;
+ timer.textContent =`${second}`;
+ timerStart();
 }
 
-//Invoke the shuffle function
+// Invoke the shuffle function
 shuffle(cardList);
 
 // Timer function by SLaks from: https://stackoverflow.com/questions/2604450/how-to-create-a-jquery-clock-timer
 function timerStart() {
-	second = 0;
-	startTime = setInterval(function(){
-	second = second + 1;
-	timer.innerHTML = second;
-	},1000);
+ second = 0;
+ startTime = setInterval(function(){
+  second = second + 1;
+  timer.innerHTML = second;
+ },1000);
 }
 
-//Create the main game function
+// Create the main game function
 function clickOnCards(card){
 
-//Create card click event
-card.addEventListener("click", function(){	
+// Create card click event
+card.addEventListener("click", function(){  
 
-//Add an if statement so that we can't open more than two cards at the same time
-	if (openedCards.length === 0 || openedCards.length === 1){
-	secondCard = this;
-	firstCard = openedCards[0];
+// Add an if statement so that we can't open more than two cards at the same time
+ if (openedCards.length === 0 || openedCards.length === 1){
+  secondCard = this;
+  firstCard = openedCards[0];
 
-	//We have opened card
-	if(openedCards.length === 1){
-	card.classList.add("open","show","disable");
-	openedCards.push(this);
+  //We have opened card
+  if(openedCards.length === 1){
+   card.classList.add("open","show","disable");
+   openedCards.push(this);
 
-	//We invoke the function to compare two cards
-	check(secondCard, firstCard);
-
-	}else {
+    //We invoke the function to compare two cards
+   check(secondCard, firstCard);
+  }else {
     //We don't have opened cards
-	card.classList.add("open","show","disable");
-	openedCards.push(this);
-    }
-	}
+    card.classList.add("open","show","disable");
+    openedCards.push(this);
+  }
+ }
 });
 }
 
-//Create the function which compares two cards
+// Create the function which compares two cards
 function check(secondCard, firstCard){
-	if(secondCard.innerHTML === firstCard.innerHTML){
+ if(secondCard.innerHTML === firstCard.innerHTML){
 
-		//Cards match
-		secondCard.classList.add("match");
-		firstCard.classList.add("match");
-		matchedCards.push(secondCard,firstCard);
-		openedCards = [];
+  //Cards match
+  secondCard.classList.add("match");
+  firstCard.classList.add("match");
+  matchedCards.push(secondCard,firstCard);
+  openedCards = [];
 
-		//If the game is over run the popup
-		gameOver();
+  //If the game is over run the popup
+  gameOver();
 
-	} else {
-		//Wait for 500 miliseconds before closing the cards
-		setTimeout(function(){
-		secondCard.classList.remove("open", "show", "disable");
-		firstCard.classList.remove("open", "show", "disable");
-		openedCards = [];
-		}, 500);
-	}	
-	
-//Count moves
+ } else {
+
+ //Wait for 500 miliseconds before closing the cards
+  setTimeout(function(){
+   secondCard.classList.remove("open", "show", "disable");
+   firstCard.classList.remove("open", "show", "disable");
+   openedCards = [];
+   }, 500);
+ }   
+    
+// Count moves
 countMoves();
 
-//Run the rating function
+// Run the rating function
 rating();
 };
-//Create moves counter
+
+// Create moves counter
 function countMoves(){
-	moves++;
-	moveCounter.innerHTML = moves;
+ moves++;
+ moveCounter.innerHTML = moves;
 }
 
-//Create star rating
+// Create star rating
 function rating() {
-    if (moves < 12) {
-		stars.innerHTML = '<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>';
-	} else if (moves > 12 && moves <15) {
-		stars.innerHTML = '<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>';
-	} else if (moves > 15){
-		stars.innerHTML = '<li><i class="fa fa-star"></i></li>';
-	}
+ if (moves < 12) {
+  stars.innerHTML = '<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>';
+ } else if (moves > 12 && moves <15) {
+  stars.innerHTML = '<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>';
+ } else if (moves > 15){
+  stars.innerHTML = '<li><i class="fa fa-star"></i></li>';
+ }
 }
 
-//Create the game over modal
+// Create the game over modal
 function gameOver (){
-	if(matchedCards.length === cardList.length){
-	   clearInterval(startTime);
+ if(matchedCards.length === cardList.length){
+  clearInterval(startTime);
 
-	   //Define final variables
-	   let timeScore = timer.innerHTML;
-	   let movesScore = moveCounter.innerHTML;
-	   let starsScore = stars.innerHTML;
+  // Define final variables
+  let timeScore = timer.innerHTML;
+  let movesScore = moveCounter.innerHTML;
+  let starsScore = stars.innerHTML;
 
-	   //Show the modal
-	   modal.classList.add("show");
+  // Show the modal
+  modal.classList.add("show");
 
-	   //Show the final variables
-	   document.getElementById("timeScore").innerHTML = second;
-	   document.getElementById("movesScore").innerHTML = moves;
-	   document.getElementById("starsScore").innerHTML = stars.innerHTML;
-	   replay.addEventListener("click",function(){
-			deck.innerHTML = "";
-			openedCards = [];
-			matchedCards = [];
-			moves = 0;
-			moveCounter.innerHTML = 0;
-			stars.innerHTML = '<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>';
-			start();
-			modal.classList.remove("show");
-		});
-	   }
+  // Show the final variables
+  document.getElementById("timeScore").innerHTML = second;
+  document.getElementById("movesScore").innerHTML = moves;
+  document.getElementById("starsScore").innerHTML = stars.innerHTML;
+  replay.addEventListener("click",function(){
+    deck.innerHTML = "";
+    openedCards = [];
+    matchedCards = [];
+    moves = 0;
+    moveCounter.innerHTML = 0;
+    stars.innerHTML = '<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>';
+    start();
+    modal.classList.remove("show");
+  });
+ }
 }
 
-//Create the restart button
+// Create the restart button
 const restart = document.querySelector(".restart");
+
 restart.addEventListener("click",function(){
-	deck.innerHTML = "";
-	openedCards = [];
-	matchedCards = [];
-	moves = 0;
-	moveCounter.innerHTML = 0;
-	stars.innerHTML = '<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>';
-	shuffle(cardList);
-	start();
+ deck.innerHTML = "";
+ openedCards = [];
+ matchedCards = [];
+ moves = 0;
+ moveCounter.innerHTML = 0;
+ stars.innerHTML = '<li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li><li><i class="fa fa-star"></i></li>';
+ shuffle(cardList);
+ start();
 });
 
-//Resets the timer when the game is restarted
+// Resets the timer when the game is restarted
 function resetTimer(timer) {
-    if (timer) {
-        clearInterval(timer);
-    }
+ if (timer) {
+  clearInterval(timer);
+ }
 }
 
-//Invoke the start function
+// Invoke the start function
 start();
 
 /*
